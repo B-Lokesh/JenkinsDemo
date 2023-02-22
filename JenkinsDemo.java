558810226b65ed08bf8e6a14345c1057a8afa884
BATCHU LOKESH <84063366+B-Lokesh@users.noreply.github.com>
2023-02-22 23:15:05 +0530

    Update JenkinsDemo.java

--- JenkinsDemo.java
+++ JenkinsDemo.java
@@ -1,22 +1,10 @@
 import java.util.*;
 import java.lang.*;
-public class Pattern4 {
+public class JenkinsDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 for(int i=1;i<=4;i++)
-		 {
-			 for(int j=3;j>=i;j--)
-			 {
-				 System.out.print(" ");
-			 }
-			 for(int k=1;k<=i;k++)
-			 {
-				 System.out.print("*");
-			 }
-			 System.out.println();
-		 }
-
+		System.out.print("Hello github");
+		
 	}
-
 }
